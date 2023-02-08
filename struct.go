25a6c57e3caf067f90eec407e11b1a4f225c1be8
Opssy <opeyemib39@gmail.com>
2023-02-08 12:48:05 +0100

    rebased

--- struct.go
+++ struct.go
@@ -1,38 +1,65 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type shape interface {
-	area() float64
+//
+//type shape interface {
+//	area() float64
+//}
+//
+//type rect struct {
+//	width, height float64
+//}
+//type circle struct {
+//	radius float64
+//}
+//
+//func (r rect) area() float64 {
+//	return r.width * r.height
+//}
+//func (c circle) area() float64 {
+//	return math.Pi * c.radius * c.radius
+//}
+//func printArea(sh shape) {
+//	fmt.Printf("%v area is %v\n", sh, sh.area())
+//}
+//func main() {
+//	r := rect{
+//		width:  3,
+//		height: 5,
+//	}
+//	c := circle{
+//		radius: 5,
+//	}
+//	printArea(r)
+//	printArea(c)
+//}
+
+type Engineer struct {
+	Name    string
+	Age     int
+	Project Project
 }
 
-type rect struct {
-	width, height float64
-}
-type circle struct {
-	radius float64
+type Project struct {
+	Name         string
+	Priority     string
+	Technologies []string
 }
 
-func (r rect) area() float64 {
-	return r.width * r.height
-}
-func (c circle) area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-func printArea(sh shape) {
-	fmt.Printf("%v area is %v\n", sh, sh.area())
-}
 func main() {
-	r := rect{
-		width:  3,
-		height: 5,
+
+	engineer := Engineer{
+		Name: "Elliot",
+		Age:  27,
+		Project: Project{
+			Name:         "Beginner's Guide to Go",
+			Priority:     "High",
+			Technologies: []string{"Go"},
+		},
 	}
-	c := circle{
-		radius: 5,
-	}
-	printArea(r)
-	printArea(c)
+	fmt.Printf("%+v\n", engineer)
+
+	fmt.Println(engineer.Project.Name)
+
 }
